77d278770cc29ad208f27195250cf295c3d0dd3b
QasimKhatri <100308376+QasimKhatri@users.noreply.github.com>
2022-06-15 04:11:16 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {System.out.println("Hello World");System.out.println("Qasim Khatri");System.out.println("17226 Stratford Green Dr.");System.out.println("Sugar Land, TX 77498");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javacPayroll.java
@@ -0,0 +1,13 @@
+
+public class javacPayroll {
+
+
+   public static void main(String[] args)
+   {
+      int hours = 50;
+      double grossPay, payRate = 22.5;
+
+      grossPay = hours * payRate;
+      System.out.println("Your gross pay is $" + grossPay);
+   }
+}
